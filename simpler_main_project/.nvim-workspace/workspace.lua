7c0839e5d4c4539eb9a38257f3557ffeb14e437e
Oskari Kristian Tuormaa <oskaritu@gmail.com>
2024-12-01 17:46:18 +0100

    Replace : with <cmd> in workspace

--- simpler_main_project/.nvim-workspace/workspace.lua
+++ simpler_main_project/.nvim-workspace/workspace.lua
@@ -2,8 +2,8 @@ local nmap = require('config.utils').nmap
 
 local pico_path = "PICO_SDK_PATH=(realpath ../3rdparty/pico-sdk)"
 
-nmap('<leader>e', ':sp term://cmake --build build -j30<cr>')
-nmap('<leader>bp', ':sp term://' .. pico_path .. ' cmake --fresh -B build && cmake --build build -j30<cr>')
-nmap('<leader>E', ':sp term://./program.sh && ./reset.sh<cr>')
-nmap('<leader>dr', ':sp term://./reset.sh<cr>')
-nmap('<leader>de', ':sp term://./flash_erase.sh<cr>')
+nmap('<leader>e', '<cmd>sp term://cmake --build build -j30<cr>')
+nmap('<leader>bp', '<cmd>sp term://' .. pico_path .. ' cmake --fresh -B build && cmake --build build -j30<cr>')
+nmap('<leader>E', '<cmd>sp term://./program.sh && ./reset.sh<cr>')
+nmap('<leader>dr', '<cmd>sp term://./reset.sh<cr>')
+nmap('<leader>de', '<cmd>sp term://./flash_erase.sh<cr>')
